d1e5a28ec93523d4388fab03fa634c9e0759dcb2
felipeolivaresr <olivaresroblesfelipe@gmail.com>
2024-07-14 19:22:51 -0400

    Integración de Jq a sección quienes somos

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+
+    $(".Toggleoff").click(function(){
+        $(".cubo").toggle(); 
+
+
+
+
+    });
+
+
+
+
+
+    $(".send").click(function(){ 
+        alert("Su mensaje fue enviado correctamente...");
+    });
+
+
+});
