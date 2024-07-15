1ca72d9c1b6b27acf095b5a390ad2076dba251fb
felipeolivaresr <olivaresroblesfelipe@gmail.com>
2024-07-14 20:29:34 -0400

    proyecto final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 
-    $(".Toggleoff").click(function(){
-        $(".cubo").toggle(); 
+    $(".Toggleoff").click(function () {
+        $(".cubo").toggle();
 
 
 
 
     });
 
+    $("img").each(function () {
+        var $this = $(this);
+        $this.data('originalWidth', $this.width());
+        $this.data('originalHeight', $this.height());
+    });
+
+    $("img").click(function () {
+        var $this = $(this);
+        var originalWidth = $this.data('originalWidth');
+        var originalHeight = $this.data('originalHeight');
+
+        $this.animate({
+            width: "300px",
+            height: "300px",
+            opacity: 0.5
+        }, 1000, function () {
+            // Volver a las propiedades originales
+            $this.animate({
+                width: originalWidth,
+                height: originalHeight,
+                opacity: 1
+            }, 1000);
+        });
+    });
 
 
-
-
-    $(".send").click(function(){ 
+    $(".send").click(function () {
         alert("Su mensaje fue enviado correctamente...");
     });
 
